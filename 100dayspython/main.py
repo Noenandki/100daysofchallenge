7a47190b063e54d9d10ffe950947048d0b9776ad
Noenandki <155680841+Noenandki@users.noreply.github.com>
2024-07-05 08:52:54 +0200

    Add files via upload

---
+++ 100dayspython/main.py
@@ -0,0 +1,12 @@
+print("Hello " + input("what is your name ?" "!"))
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+#print("Hello " + input("what is your name ?" "!"))  
+
+name = input("what is your name ?")
+print(name )
+
+name = input("What is your name ?")
+length = len(name)
+print(length)
